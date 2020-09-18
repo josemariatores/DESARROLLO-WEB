4d1959c1ec0aa830db967644c77acb86aeea7c85
josemaria9 <josemaria070500@gmail.com>
2020-09-18 13:29:48 +0200

    2

---
+++ AE1-2.js
@@ -0,0 +1,18 @@
+
+// Array  12 meses del año
+var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+console.log(
+(meses[0])
+(meses[1])
+(meses[2])
+(meses[3])
+(meses[4])
+(meses[5])
+(meses[6])
+(meses[7])
+(meses[8])
+(meses[9])
+(meses[10])
+(meses[11])
+);
+
