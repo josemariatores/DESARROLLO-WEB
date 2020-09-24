3039ddab6d53cb0b771ef95db8df7071762f7801
josemaria9 <josemaria070500@gmail.com>
2020-09-24 13:00:43 +0200

    Cambios en EJ6

--- AE1-6.js
+++ AE1-6.js
@@ -1,3 +1,4 @@
+//si lo hacemos asi saldria en pantalla
 var numero;
 numero=prompt('Ingrese valor del numero:','');
 if (numero % 2 == 0)
@@ -5,3 +6,4 @@ if (numero % 2 == 0)
   document.write('El numero introducido es par');
 }
 else {document.write('El numero introducido es impar');}
+
