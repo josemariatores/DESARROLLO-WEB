fd3706d084d8a7cc71c82f8a37f67ecde10cd4ac
josemaria9 <josemaria070500@gmail.com>
2020-09-24 12:13:02 +0200

    ejercicio 6

---
+++ AE1-6.js
@@ -0,0 +1,7 @@
+var numero;
+numero=prompt('Ingrese valor del numero:','');
+if (numero % 2 == 0)
+{
+  document.write('El numero introducido es par');
+}
+else {document.write('El numero introducido es impar');}
