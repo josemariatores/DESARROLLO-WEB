4fc82b43c53c28731fe0f3c404a4bf88e170d037
josemaria9 <josemaria070500@gmail.com>
2020-09-24 12:12:22 +0200

    Ejercicio 2

---
+++ AE1-2.js
@@ -0,0 +1,6 @@
+
+// Array  12 meses del año
+ const varmeses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+//Asi listado
+console.log(varmeses)
+//Asi hacemos resultados en tablas
