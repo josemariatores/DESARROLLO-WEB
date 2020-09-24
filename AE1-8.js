2dbe697bc082ce6ebc4c111dc7eb58eaa0648521
josemaria9 <josemaria070500@gmail.com>
2020-09-24 12:13:18 +0200

    Ejercicio 8

---
+++ AE1-8.js
@@ -0,0 +1,10 @@
+var numeros
+numeros=prompt('introduce tu numero','');
+if(parseint(numeros>12)){
+    alert("Ese numero no es valido vuelve a probar")
+
+}else if(numeros=12){
+    alert ("es valido el numero")
+}else if(parseint(numeros<12)){
+    alert ("Es valido el numero")
+}
