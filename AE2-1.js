3c7869df53b2f80a7a4ec724eb92bc60a4a7d5b1
josemaria9 <josemaria070500@gmail.com>
2020-10-01 10:11:50 +0200

    actualizamos 2-1

--- AE2-1.js
+++ AE2-1.js
@@ -1,1 +1,17 @@
 valores=["Ana",2,"Javi",5,"Roberto",7,"Vanessa",10];
+
+function eliminamostexto(valores){
+    valores = valores.filter(name => typeof(name) == typeof("text"));
+    console.log(valores);
+    return valores;
+}
+console.log(eliminamostexto(valores));
+// no funciona revisar mas tarde
+
+eliminamostexto(valores);
+function eliminamosnumeros(valores){
+    valores = valores.filter(name => typeof(name)==typeof(1)&& !(name%2==0) || (typeof(name)==typeof("text")));
+    console.log(valores);
+    return (valores);
+}
+console.log(eliminamosnumeros(valores));
